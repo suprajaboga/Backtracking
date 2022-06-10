2c49a9040d62228fa1aa3a483bd1ff1ce02fdfbc
suprajaboga <97597175+suprajaboga@users.noreply.github.com>
2022-06-10 11:47:30 +0530

    File updated

---
+++ combinationsum3/combinationsum3.java
@@ -0,0 +1,34 @@
+package combinationsum3;
+import java.util.*;
+
+public class combinationsum3 {
+	public static void print(int start, int end, int target, int k, List<Integer> temp, List<List<Integer>> res)
+    {
+        if(temp.size() == k && target == 0)
+        {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=start;i<=end;i++)
+        {
+            temp.add(i);
+            print(i+1,end,target-i,k,temp,res);
+            temp.remove(temp.size()-1);
+        }
+    }
+	public static List<List<Integer>> combine(int target, int k) 
+    {
+        List<Integer> temp = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        print(1,9,target,k,temp,res);
+        return res;
+    }
+	public static void main(String args[])
+	{
+		int target = 9, k = 3;
+		List<List<Integer>> ans = combine(target,k);
+		System.out.println(ans);
+	}
+
+
+}
