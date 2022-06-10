7329669db83268ff99022d9c23faa28d4c14b863
suprajaboga <97597175+suprajaboga@users.noreply.github.com>
2022-06-10 10:45:34 +0530

    Add files via upload

---
+++ generateparanthesis/generateparanthesis.java
@@ -0,0 +1,31 @@
+package generateparanthesis;
+
+import java.util.*;
+
+public class generateparanthesis 
+{
+	public static void findAll(String current, int open, int close, List<String> result, int n)
+	{
+		if(current.length() == 2*n)
+		{
+			result.add(current);
+			return;
+		}
+		if(open < n)
+			findAll(current+"(", open+1, close, result, n);
+		if(close < open)
+			findAll(current+")", open, close+1, result, n);
+	}
+	public static List<String> generateParanthesis(int n)
+	{
+		List<String> res = new ArrayList<>();
+        findAll("(",1,0,res,n);
+        return res;
+	}
+	public static void main(String args[])
+	{
+		int n = 3;
+		List<String> ans = generateParanthesis(n);
+		System.out.println(ans);
+	}
+}
